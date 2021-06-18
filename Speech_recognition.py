d7b97728dff1e76936ffa76314af7c0d1a8d3813
root <elyaakouby@gmail.com>
2021-06-18 11:28:54 +0200

    push all

---
+++ Speech_recognition.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import sys
+import os
+import speech_recognition as sr
+
+#path = sys.argv[1]
+#LANG = sys.argv[2]
+
+ScriptName =  os.path.basename(sys.argv[0])
+
+def RecognizerFunction():
+    # Check if path exits
+    #if os.path.exists(path):
+
+    recognizer = sr.Recognizer()
+
+    ''' recording the sound '''
+
+    #with sr.AudioFile("./speech.wav") as source:
+    with sr.AudioFile(sys.argv[1]) as source:
+        recorded_audio = recognizer.listen(source)
+        print("Done recording")
+
+    ''' Recorgnizing the Audio '''
+    try:
+        print ('Voice recognition in progress..')
+        text = recognizer.recognize_google(
+                recorded_audio, 
+                #language="en-US"
+                language="sys.argv[2]"
+            )
+        print("Decoded Text : {}".format(text))
+
+    except Exception as ex:
+        print(ex)
+
+NbrArges = len(sys.argv)
+if NbrArges == 3:
+    RecognizerFunction()
+else:
+    blah = 'blah'
+    print ('\n' +ScriptName+ ' <inputfile> <Language>')
+    print ('\n  -> language list : en_US, fr_FR, ar_AE..\n')
+    sys.exit()
+
